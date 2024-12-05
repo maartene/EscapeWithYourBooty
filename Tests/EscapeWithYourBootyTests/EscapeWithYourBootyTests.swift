2c5e1371857500e87782ea7c9cf5cec5d896fd3f
Maarten Engels <maartene@thedreamweb.eu>
2024-12-05 14:54:14 +0100

    T: we should know that a sea without any navy is safe

--- Tests/EscapeWithYourBootyTests/EscapeWithYourBootyTests.swift
+++ Tests/EscapeWithYourBootyTests/EscapeWithYourBootyTests.swift
@@ -2,8 +2,15 @@ import Testing
 @testable import EscapeWithYourBooty
 
 @Suite("To make sure we get our bounty safely to shore") struct EscapeWithYourBootyTests {
-    @Test("we should know that a 1x1 sea without any navy is safe") func aSafeRouteOf1x1WithoutNavy() {
-        let sea: [[Character]] = [["X"]]
+    @Test("we should know that a sea without any navy is safe", arguments: [
+        [
+            ["X"]
+        ],
+        [
+            ["X", "0"],
+            ["0", "0"],
+        ]
+    ]) func aSeaWithoutAnyNavyIsSafe(sea: [[Character]]) {
         #expect(isThisASafeRoute(in: sea) == true)
     }
     
