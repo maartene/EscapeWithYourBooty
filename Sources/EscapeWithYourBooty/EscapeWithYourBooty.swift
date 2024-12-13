f35eddd18447f8389c4190a242dd8d71ac80cd73
Pim Nijman <pim.nijman@ns.nl>
2024-12-13 14:29:53 +0100

    R: Use typealias for Coordinate

--- Sources/EscapeWithYourBooty/EscapeWithYourBooty.swift
+++ Sources/EscapeWithYourBooty/EscapeWithYourBooty.swift
@@ -9,6 +9,8 @@ extension Character {
     static let navyShip: Character = "N"
 }
 
+typealias Coordinate = (y: Int, x: Int)
+
 func isThisASafeRoute(in sea: [[Character]]) -> Bool {
     var result = true
     for surrounding in surroundings(of: determinePirateShipPosition(sea: sea), in: sea) {
@@ -19,16 +21,16 @@ func isThisASafeRoute(in sea: [[Character]]) -> Bool {
     return result
 }
 
-private func surroundings(of shipPosition: (Int, Int), in sea: [[Character]]) -> [(Int, Int)] {
+private func surroundings(of shipPosition: Coordinate, in sea: [[Character]]) -> [Coordinate] {
     let seaHeight = sea.count
     let seaWidth = sea[0].count
-    let offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1,1)]
+    let offsets: [Coordinate] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1,1)]
     return offsets
-        .map { offset in (shipPosition.0 + offset.0, shipPosition.1 + offset.1) }
+        .map { offset in (shipPosition.x + offset.x, shipPosition.y + offset.y) }
         .filter { isWithinSea($0, seaWidth, seaHeight) }
 }
 
-private func determinePirateShipPosition(sea: [[Character]]) -> (Int, Int) {
+private func determinePirateShipPosition(sea: [[Character]]) -> Coordinate {
     let shipPosition = (10, 10)
     for y in 0..<sea.count {
         for x in 0..<sea[y].count {
@@ -41,8 +43,8 @@ private func determinePirateShipPosition(sea: [[Character]]) -> (Int, Int) {
     return shipPosition
 }
 
-private func isWithinSea(_ surrounding: (Int, Int), _ seaWidth: Int, _ seaHeight: Int) -> Bool {
-    surrounding.0 >= 0 && 
+private func isWithinSea(_ surrounding: Coordinate, _ seaWidth: Int, _ seaHeight: Int) -> Bool {
+    surrounding.0 >= 0 &&
         surrounding.0 < seaHeight && 
         surrounding.1 >= 0 && 
         surrounding.1 < seaWidth
